b18a13d235f992fec50ae007824082f5d0076c24
rhdmstj17@cau.ac.kr <rhdmstj17@cau.ac.re>
2020-07-25 02:52:48 +0900

    'unit19'

---
+++ askcompony/askcompony/shop/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+
+class Item(models.Model):
+    name =models.CharField(max_length=100)
+    desc = models.TextField(blank=True)
+    price = models.PositiveIntegerField()
+    is_publish =models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        #return '<{}> {}'.format(self.pk, self.name)
+        return f'<{self.pk}> {self.name}'
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
